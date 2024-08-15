2d52df29b2d594f4cb00384465c95d3ef7b0ac66
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-08-16 00:03:02 +0300

    product list add

--- scripts/Product/product.js
+++ scripts/Product/product.js
@@ -1,3 +1,25 @@
-import base_url from "../api";
+import base_url from "../api.js";
+import { capitalizeFirstLetter } from "../utils/firsletter.js";
 
-export async function getProducts() {}
+const productList = document.querySelector(".product-list");
+
+export async function getProducts() {
+  const response = await fetch(`${base_url}`);
+  const data = await response.json();
+
+  data.forEach((item) => {
+    productList.innerHTML += `<div class="card" id="${item.id}">
+            <img
+              class="product-img"
+              src="${item.image}"
+              alt="img"
+            />
+            <h1 class="card-title">${capitalizeFirstLetter(item.title)}</h1>
+            <div class="card-category">${capitalizeFirstLetter(
+              item.category
+            )}</div>
+            
+            <div class="card-price">$${item.price}</div>
+          </div>`;
+  });
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 import { getCategories } from "./category.js";
+import { getProducts } from "./Product/product.js";
 import { initializeSlider } from "./slider.js";
 
-window.addEventListener("DOMContentLoaded", initializeSlider);
-window.addEventListener("DOMContentLoaded", getCategories);
+window.addEventListener("DOMContentLoaded", () => {
+  initializeSlider(), getCategories(), getProducts();
+});
