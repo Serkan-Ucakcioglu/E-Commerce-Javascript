0a65a3be6cd831669a76803d2d3aa9dc261a8e09
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-08-15 21:56:17 +0300

    check add

--- scripts/category.js
+++ scripts/category.js
@@ -2,14 +2,14 @@ import base_url from "./api.js";
 import { capitalizeFirstLetter } from "./utils/firsletter.js";
 
 const categoryDiv = document.querySelector(".check");
-
+let nulls = [];
 export async function getCategories() {
   const response = await fetch(`${base_url}/categories`);
   const data = await response.json();
 
   data.forEach((item) => {
-    categoryDiv.innerHTML += `<div class="category-item">
-              <input type="checkbox" id="${item}" class="a" />
+    categoryDiv.innerHTML += `<div class="category-item" data-item-name="${item}">
+              <input type="checkbox" id="${item}" class="category-checkbox" />
               <label for="${item}" class="checkbox"></label>
                  <label for="${item}" class="item-title">
              ${capitalizeFirstLetter(item)}
@@ -17,3 +17,23 @@ export async function getCategories() {
     </div>`;
   });
 }
+categoryDiv.addEventListener("click", (event) => {
+  if (event.target.matches("input[type='checkbox']")) {
+    const checkbox = event.target;
+
+    const allCheckboxes = document.querySelectorAll(".category-checkbox");
+
+    allCheckboxes.forEach((cb) => {
+      if (cb !== checkbox) {
+        cb.checked = false;
+        cb.classList.remove("on");
+      }
+    });
+
+    if (checkbox.checked) {
+      checkbox.classList.add("on");
+    } else {
+      checkbox.classList.remove("on");
+    }
+  }
+});
