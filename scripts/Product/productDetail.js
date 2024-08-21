36b3e5561822a63b6731946116fe8a35a1c68038
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-08-21 23:19:55 +0300

    update

--- scripts/Product/productDetail.js
+++ scripts/Product/productDetail.js
@@ -9,55 +9,66 @@ const productElement = document.querySelector(".products .container");
 
 async function getProductDetail() {
   try {
-    const [data1, data2] = await Promise.all([
+    let [data1, categoryData] = await Promise.all([
       useFetch(`${productId}`),
       useFetch(`category/${category}`),
     ]);
+    console.log(data1, "d1");
+    console.log(categoryData, "d2");
+
     productElement.innerHTML = `
     <div class="tree-product">
-     <div class="product-detail" id=${data1.id}>
-  <div class="product-images">
-            
-            <img
-              width="400px"
-              heigh="400px"
-              src=${data1.image}
-              alt="image"
-            />
-          </div>
-          <div class="details-product">
+      <div class="product-detail" id=${data1.id}>
+        <div class="product-images">
+          <img
+            width="400px"
+            height="400px"
+            src="${data1.image}"
+            alt="image"
+          />
+        </div>
+        <div class="details-product">
           <h3>${capitalizeFirstLetter(data1.title)}</h3>
-            <div class="product-description">
-<strong>Description</strong>:${capitalizeFirstLetter(data1.description)}
-            </div>
-            <span><strong>Price: </strong>$${data1.price}</span>
-            <span><strong>Price: </strong> ${capitalizeFirstLetter(
-              data1.category
-            )}</span>
-            <span> <strong>Rating: </strong> ${data1.rating.rate} (${
+          <div class="product-description">
+            <strong>Description</strong>: ${capitalizeFirstLetter(
+              data1.description
+            )}
+          </div>
+          <span><strong>Price: </strong> $${data1.price}</span>
+          <span><strong>Category: </strong> ${capitalizeFirstLetter(
+            data1.category
+          )}</span>
+          <span><strong>Rating: </strong> ${data1.rating.rate} (${
       data1.rating.count
     } reviews)</span>
-          </div>
-          
-          </div>
-          <ul class="similar-category">
-      <li class="similar-card">
+        </div>
+      </div>
+      <ul class="similar-category"></ul>
+    </div>
+    `;
+
+    let similarCategory = document.querySelector(".similar-category");
+
+    categoryData.forEach((item) => {
+      console.log(item, "item");
+
+      similarCategory.innerHTML += `
+      <li class="similar-card" id=${item.id}>
         <img
           width="100px"
           height="100px"
-          src="https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg"
+          src="${item.image}"
           alt="image"
         />
-        <h3>Product name</h3>
+        <h5 class="card-title">${capitalizeFirstLetter(item.title)}</h5>
         <div>
-          <span>Category Name</span>
-          <span>Price</span>
+          <span> ${capitalizeFirstLetter(item.category)} </span>
+          <span class="card-price">$${item.price}</span>
         </div>
-      </li>
-    </ul></div>
- `;
+      </li>`;
+    });
   } catch (error) {
-    alert(error);
+    console.error("Error fetching product details:", error);
   }
 }
 
