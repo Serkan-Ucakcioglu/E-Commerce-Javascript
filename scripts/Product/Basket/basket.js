5e316a3659453ac04b5521de1bff4b697af356ac
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-08-26 23:07:17 +0300

    update

--- scripts/Product/Basket/basket.js
+++ scripts/Product/Basket/basket.js
@@ -1,5 +1,15 @@
 import { uiUtils } from "../../utils/ui-utils.js";
+import { getCard } from "../Card/card.js";
 
 const basketList = document.querySelector(".buy-product");
 
+function basketLists() {
+  const card = getCard();
+  console.log(card);
+
+  //   card.map((item) => {
+  //     return (basketList.innerHTML = `<div>${item.title}</div>`);
+  //   });
+}
+basketLists();
 window.addEventListener("DOMContentLoaded", uiUtils);
