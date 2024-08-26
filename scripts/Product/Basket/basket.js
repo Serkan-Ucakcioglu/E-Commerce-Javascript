09dfb51bd9cd35ca8c1072007b71fb5090161f37
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-08-26 23:25:21 +0300

    update

--- scripts/Product/Basket/basket.js
+++ scripts/Product/Basket/basket.js
@@ -7,9 +7,26 @@ function basketLists() {
   const card = getCard();
   console.log(card);
 
-  //   card.map((item) => {
-  //     return (basketList.innerHTML = `<div>${item.title}</div>`);
-  //   });
+  card.map((item) => {
+    return (basketList.innerHTML = ` <div class="basket-card" id=${item.id}>
+            <div class="basket-product">
+              <img
+                width="100px"
+                height="100px"
+                src=${item.image}
+                alt="image"
+                loading="lazy"
+               
+              />
+              <div style="text-align:center;">
+              ${item.title}
+              <span>Remove</span>
+              </div>
+            </div>
+            <div class="basket-quantity">${item.quantity}</div>
+            <span class="basket-prices">${item.price}</span>
+          </div>`);
+  });
 }
 basketLists();
 window.addEventListener("DOMContentLoaded", uiUtils);
