6d4c9d3417f734286850e9fabb2bda8c694748a1
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-08-27 21:39:45 +0300

    update

--- scripts/Product/Basket/basket.js
+++ scripts/Product/Basket/basket.js
@@ -23,7 +23,11 @@ function basketLists() {
               </div>
             </div>
             <div class="basket-operation">
-            <div class="basket-quantity">${item.quantity}</div>
+            <div class="basket-quantity">
+            <button><svg class="fill-current text-gray-600 w-3 cursor-pointer" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg></button>
+            <input value=${item.quantity} disabled  />
+            <button><svg class="fill-current text-gray-600 w-3 cursor-pointer" viewBox="0 0 448 512"><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg></button>
+            </div>
             <span class="basket-prices">$${item.price}</span>
               <div class="remove-product">
               <svg fill="#e50606" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
