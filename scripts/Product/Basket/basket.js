0a8934c7e9079001009c95c82fde5ceb164aefbb
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-08-31 22:34:32 +0300

    svgs were removed from html and used as files and increased and decreased svgs were changed

--- scripts/Product/Basket/basket.js
+++ scripts/Product/Basket/basket.js
@@ -40,30 +40,13 @@ function basketLists() {
             </div>
             <div class="basket-operation">
             <div class="basket-quantity">
-            <button data-quantity="decrease"><svg class="fill-current text-gray-600 w-3 cursor-pointer" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg></button>
+            <button data-quantity="decrease"><img src="../../../assets/decrease.svg"   alt="decrease"></button>
             <input value=${item.quantity} disabled  />
-            <button data-quantity="increase"><svg class="fill-current text-gray-600 w-3 cursor-pointer" viewBox="0 0 448 512"><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg></button>
+            <button data-quantity="increase"><img  src="../../../assets/increase.svg" alt="increase"></button>
             </div>
             <span class="basket-prices">$${totalValue()}</span>
               <div class="remove-product" data-quantity="remove">
-              <svg fill="#e50606" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink" width="35" height="35" viewBox="0 0 408.483 408.483"
-            xml:space="preserve" stroke="#e50606">
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-            <g id="SVGRepo_iconCarrier">
-                <g>
-                    <g>
-                        <path
-                            d="M87.748,388.784c0.461,11.01,9.521,19.699,20.539,19.699h191.911c11.018,0,20.078-8.689,20.539-19.699l13.705-289.316 H74.043L87.748,388.784z M247.655,171.329c0-4.61,3.738-8.349,8.35-8.349h13.355c4.609,0,8.35,3.738,8.35,8.349v165.293 c0,4.611-3.738,8.349-8.35,8.349h-13.355c-4.61,0-8.35-3.736-8.35-8.349V171.329z M189.216,171.329 c0-4.61,3.738-8.349,8.349-8.349h13.355c4.609,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.737,8.349-8.349,8.349h-13.355 c-4.61,0-8.349-3.736-8.349-8.349V171.329L189.216,171.329z M130.775,171.329c0-4.61,3.738-8.349,8.349-8.349h13.356 c4.61,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.738,8.349-8.349,8.349h-13.356c-4.61,0-8.349-3.736-8.349-8.349V171.329z">
-                        </path>
-                        <path
-                            d="M343.567,21.043h-88.535V4.305c0-2.377-1.927-4.305-4.305-4.305h-92.971c-2.377,0-4.304,1.928-4.304,4.305v16.737H64.916 c-7.125,0-12.9,5.776-12.9,12.901V74.47h304.451V33.944C356.467,26.819,350.692,21.043,343.567,21.043z">
-                        </path>
-                    </g>
-                </g>
-            </g>
-        </svg>
+               <img src="../../../assets/remove.svg" alt="remove">
         <div>
             </div>
           </div>
