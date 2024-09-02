ec8bc4e746e84180d4d007763e0de34477eb5317
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-09-02 22:44:42 +0300

    validateEmail function created

--- scripts/Form/login-form.js
+++ scripts/Form/login-form.js
@@ -1,5 +1,5 @@
-const email = document.querySelector("#email");
-const password = document.querySelector("#password");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
 const submitBtn = document.querySelector(".submit");
 
 function showError(inputElement, message) {
@@ -13,4 +13,19 @@ function showError(inputElement, message) {
 function clearErrorMessage(inputElement) {
   let error = inputElement.parentNode.querySelector("error-message");
   if (error) return error.remove();
+}
+
+function validateEmail() {
+  const email = emailInput.value.trim();
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!email) {
+    showError(emailInput, "Email is required!");
+    return false;
+  } else if (!emailPattern.test(email)) {
+    showError(emailInput, "Please enter a valid email address.");
+    return false;
+  } else {
+    clearErrorMessage(emailInput);
+  }
 }
