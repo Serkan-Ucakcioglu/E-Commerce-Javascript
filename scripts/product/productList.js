3471a06aae34607f63e7671ba6a5bdbd8c7ec804
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-09-08 20:55:16 +0300

    pagi

--- scripts/product/productList.js
+++ scripts/product/productList.js
@@ -5,16 +5,11 @@ import { pagination, nextPage, prevPage } from "./pagination.js";
 
 const productList = document.querySelector(".product-list");
 
-export async function getProducts(query) {
-  showLoader();
-  try {
-    const data = await useFetch(query);
-    let cards = "";
-    const { newData, totalPage } = pagination(data);
-    console.log(newData);
-
-    newData.forEach((item) => {
-      cards += `
+function createCard(data) {
+  productList.innerHTML = "";
+  let cards = "";
+  data.forEach((item) => {
+    cards += `
       <div class="card" id=${item.id}>
         <div class="card-img">
           <img
@@ -35,13 +30,34 @@ export async function getProducts(query) {
           <a href=${`product.html?id=${item.id}&category=${encodeURIComponent(
             item.category
           )}`} class="view-btn" data-id="${item.id}" data-category="${
-        item.category
-      }">View Product</a>
+      item.category
+    }">View Product</a>
         </div>
+        <button data-pagi="next">nextpage<button>
         </div>
       `;
+  });
+  productList.innerHTML = cards;
+}
+
+export async function getProducts(query) {
+  showLoader();
+  try {
+    const data = await useFetch(query);
+
+    let { newData, totalPage } = pagination(data);
+    createCard(newData);
+
+    const nextButton = document.querySelector("[data-pagi]");
+
+    nextButton.addEventListener("click", () => {
+      nextPage(data.length);
+      let { newData: news } = pagination(data);
+      newData = news;
+      console.log(news, "news");
+
+      createCard(news);
     });
-    productList.innerHTML = cards;
   } catch (error) {
     productList.innerHTML = `<p>Error loading products. Please try again later.</p>`;
   } finally {
