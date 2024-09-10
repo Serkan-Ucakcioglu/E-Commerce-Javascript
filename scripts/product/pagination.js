ba19930c3251d445e71fcc5ddb236791039aa554
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-09-10 21:11:24 +0300

    deletion of unnecessary controls

--- scripts/product/pagination.js
+++ scripts/product/pagination.js
@@ -20,26 +20,16 @@ function changePage(newPage, data, length) {
 }
 
 function nextPage(length, data) {
-  prevButton.disabled = false;
-  console.log("click");
-
   if (currentPage !== length) {
-    nextButton.disabled = false;
     currentPage += 1;
     changePage(currentPage, data, length);
-  } else {
-    nextButton.disabled = true;
   }
 }
 
 function prevPage(data, length) {
-  nextButton.disabled = false;
   if (currentPage - 1 !== 0) {
-    prevButton.disabled = false;
     currentPage -= 1;
     changePage(currentPage, data, length);
-  } else {
-    prevButton.disabled = true;
   }
 }
 
