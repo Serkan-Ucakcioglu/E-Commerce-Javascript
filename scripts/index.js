7f95fb5f8743ecd58895f5fabd8b6f5726ac44a2
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-09-14 22:15:49 +0300

    import name update

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,12 @@ import { getCategories } from "./category.js";
 import { getBasketItemCount } from "./product/card-operation/card-function.js";
 import { getProducts } from "./product/productList.js";
 import { initializeSlider } from "./slider.js";
-import { createHeader, updateBasketCount } from "./utils/header.js";
+import { createHeader, updateHeaderBasketCount } from "./utils/header.js";
 
 function init() {
   initializeSlider(), getCategories(), getProducts("products/");
   createHeader();
-  updateBasketCount(getBasketItemCount());
+  updateHeaderBasketCount(getBasketItemCount());
 }
 
 window.addEventListener("DOMContentLoaded", init);
